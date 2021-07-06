5cba93206e514d0b874b6bd08cad204f31412b2e
idiotequelee <78372620+idiotequelee@users.noreply.github.com>
2021-07-06 18:31:41 +0300

    Update main.py

--- final/main.py
+++ final/main.py
@@ -54,8 +54,11 @@ def photo_handler(message):
 #     print(message)
 #     bot.reply_to(message, "Welcome")
 
+
+
 @bot.message_handler(content_types=["text"])
 def mark_handler(message):
+    print(message)
     msg = message.text.strip()
     if msg in ["1", "2", "3", "4", "5"]:
         bot.reply_to(message, "Дані твого самопочуття зафіксовано")
@@ -68,8 +71,27 @@ def mark_handler(message):
             all_data[chat_id].append()
         else:
             all_data[chat_id] = [(str(date.now()), msg)]
+            # global x, y
+            x = str(date.now())
+            y = msg 
+        print(x, y)
+            # return x, y
+            
         write_file(all_data)
-    
-    
+
+# print(x, y)
+        
+# def data_plot(message):
+#     all_data = read_file()
+#     chat_id = message.chat.id
+#     x_list = []
+#     y_list = []
+#     for item in all_data[chat_id]:
+#         print(item)
+#         x_list.append(item[chat_id])
+#         return x_list
+#     print(x_list)
+        
+
 schedule()
 bot.polling()
