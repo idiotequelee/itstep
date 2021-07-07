deb616437db6e6e62d1b5160ccc730bfbb73fe9e
idiotequelee <78372620+idiotequelee@users.noreply.github.com>
2021-07-07 18:51:47 +0300

    Delete final directory

--- final/db/base.py
+++
@@ -1,9 +0,0 @@
-from sqlachemy import create_engine
-from sqlachemy.orm import sessionmaker, declarative_base
-
-
-engine = create_engine('sqlite:///database.sqlite', echo=True)
-
-Session = sessionmaker(bind=engine)
-
-Base = declarative_base()
